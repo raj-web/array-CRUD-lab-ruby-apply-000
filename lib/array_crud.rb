00098488359d83874dcacb39721b9cb911ac1234
Viraj Ransing <rajhoustontexas@gmail.com>
2019-07-06 18:28:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["keto", "kaali", "ellie", "miko" ]
+  cats= ["keto", "kaali", "ellie", "miko" ]
 end
 
 def add_element_to_end_of_array(array, element)
-  .push ("dick")
+  cats.push ("dick")
 end
 
 def add_element_to_start_of_array(array, element)
